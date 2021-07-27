bc494ad29ea7aafb13462027d45fecd86d11a705
Fredric McWilliams <fredmcwilliams92@gmail.com>
2021-07-27 17:15:08 -0500

    working on initial folder/file setup

---
+++ models/Thought.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const ThoughtSchema = new Schema({
+  username: {
+    type: String,
+    required: 'Username is requiered!',
+  },
+  thoughText: {
+    type: String,
+    required: 'Email is already in use!',
+    match: [/.+@.+\,.+/],
+    min: 1,
+    max: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => {
+      return moment(createdAtVal).format('MMMM Do YYYY, h:mm:ss a');
+    }
+  },
+  toJSON: {
+    virtuals: true
+  },
+  id: false
+});
---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: 'Username is requiered!',
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: 'Email is already in use!',
+    match: [/.+@.+\,.+/]
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  userCreated: {
+    type: Date,
+    default: Date.now
+  }
+  },
+  {
+  toJSON: {
+    virtuals: true
+  },
+  id: false
+});
+
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
